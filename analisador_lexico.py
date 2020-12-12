8b0cd7c4adc597a60fe1b66d3ea2f25c8dad5f3d
gabbrieu <gabrielhmendes@hotmail.com>
2020-12-12 16:44:05 -0300

    Funções iniciais

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -1,1 +1,18 @@
-teste
+import sys
+import os.path
+
+class Analisador_lexico():
+    def __init__(self):
+        self.arquivo = 'programa.txt'
+
+    def tem_digito(self, dig):
+        digitos = '0123456789'
+        if dig in digitos:
+            return True
+        return False
+
+    def tem_letra(self, letra):
+        letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        if letra in letras:
+            return True
+        return False
