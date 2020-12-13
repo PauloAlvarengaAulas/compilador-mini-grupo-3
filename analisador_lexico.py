e806c819a1d0041abda3731afc9635b1ed964a66
gabbrieu <gabrielhmendes@hotmail.com>
2020-12-13 00:29:40 -0300

    Mudança para outro método

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -1,18 +1,22 @@
-import sys
 import os.path
+from plymaster.ply import *
 
 class Analisador_lexico():
-    def __init__(self):
-        self.arquivo = 'programa.txt'
-
-    def tem_digito(self, dig):
-        digitos = '0123456789'
-        if dig in digitos:
-            return True
-        return False
-
-    def tem_letra(self, letra):
-        letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        if letra in letras:
-            return True
-        return False
+    reservado = {
+        'if'	 :	'IF',
+        'else'	 :	'ELSE',
+        'then'   :  'THEN',
+        'int'	 :	'INTEGER',
+        'while'	 :	'WHILE',
+        'write'  :	'WRITE',
+        'read'   :	'READ',
+        'program':  'PROGRAM',
+        'declare':  'DECLARE',
+        'not'    :  'NOT',
+        'and'    :  'AND',
+        'do'     :  'DO',
+        'begin'  :  'BEGIN',
+        'end'    :  'END',
+        'or'     :  'OR',
+        'mod'    :  'MOD'
+    }
