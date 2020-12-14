8fe58bec7e3953547ebbaa2f2a67799d0a6118b2
gabbrieu <gabrielhmendes@hotmail.com>
2020-12-14 12:28:51 -0300

    mudança para o rply

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -1,22 +1,27 @@
-import os.path
-from plymaster.ply import *
+from rply import LexerGenerator
 
-class Analisador_lexico():
-    reservado = {
-        'if'	 :	'IF',
-        'else'	 :	'ELSE',
-        'then'   :  'THEN',
-        'int'	 :	'INTEGER',
-        'while'	 :	'WHILE',
-        'write'  :	'WRITE',
-        'read'   :	'READ',
-        'program':  'PROGRAM',
-        'declare':  'DECLARE',
-        'not'    :  'NOT',
-        'and'    :  'AND',
-        'do'     :  'DO',
-        'begin'  :  'BEGIN',
-        'end'    :  'END',
-        'or'     :  'OR',
-        'mod'    :  'MOD'
-    }
+class Analisador_Lexico():
+    def __init__(self):
+        self.lexer = LexerGenerator()
+
+    def _add_tokens(self):
+        #Parenteses
+        self.lexer.add('OPEN_PAREN', r'\(')
+        self.lexer.add('CLOSE_PAREN', r'\)')
+
+        #Ponto e vírgula
+        self.lexer.add('SEMI_COLON', r'\;')
+
+        #Operadores
+        self.lexer.add('SUM', r'\+')
+        self.lexer.add('SUB', r'\-')
+
+        #Número
+        self.lexer.add('NUMBER', r'\d+')
+
+        #Ignorar espaços
+        self.lexer.ignore(r'\s+')
+
+    def get_lexer(self):
+        self._add_tokens()
+        return self.lexer.build()
