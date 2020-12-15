0949df1783a43adbb0f128dae1203acf98587966
gabbrieu <gabrielhmendes@hotmail.com>
2020-12-14 23:51:33 -0300

    mudanças no léxico e add sintático

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -1,5 +1,5 @@
 from rply import LexerGenerator
-
+import re
 class Analisador_Lexico():
     def __init__(self):
         self.lexer = LexerGenerator()
@@ -13,23 +13,23 @@ class Analisador_Lexico():
         self.lexer.add('SEMI_COLON', r'\;')
 
         #Operadores
-        self.lexer.add('ATR', ':=')
+        self.lexer.add('ATR', r':=')
         self.lexer.add('SUM', r'\+')
         self.lexer.add('SUB', r'\-')
+        self.lexer.add('DIF',  r'<>')
+        self.lexer.add('LESS_THAN_EQ', r'<=')
+        self.lexer.add('GREATER_THAN_EQ', r'\>=')
+        self.lexer.add('<<<', r'<<<')
+        self.lexer.add('>>>', r'>>>')
+        self.lexer.add('LEFT_SHIFT', r'<<')
+        self.lexer.add('RIGHT_SHIFT', r'>>')
         self.lexer.add('LESS_THAN', r'\<')
         self.lexer.add('GREATER_THAN', r'\>')
-        self.lexer.add('LESS_THAN_EQ', r'\<=')
-        self.lexer.add('GREATER_THAN_EQ', r'\>=')
         self.lexer.add('TIMES', r'\*')
         self.lexer.add('DIV', '/')
         self.lexer.add('MOD', r'\%')
         self.lexer.add('AND', r'\&')
         self.lexer.add('OR', r'\|')
-        self.lexer.add('LEFT_SHIFT', r'<<')
-        self.lexer.add('RIGHT_SHIFT', r'>>')
-        self.lexer.add('DIF', r'<>')
-        self.lexer.add('<<<', r'<<<')
-        self.lexer.add('>>>', r'>>>')
         self.lexer.add('NOT', r'not')
 
         #Número
@@ -38,18 +38,22 @@ class Analisador_Lexico():
         #Ignorar espaços
         self.lexer.ignore(r'\s+')
 
+        #Estruturas
         self.lexer.add('IF', r'if')
         self.lexer.add('ELSE', r'else')
         self.lexer.add('END', r'end')
-
-        self.lexer.add('READ', r'read')
-        self.lexer.add('WRITE', r'write')
         self.lexer.add('DO', r'do')
         self.lexer.add('WHILE', r'while')
-        self.lexer.add('THEN', r'then')
 
+        #Interação
+        self.lexer.add('READ', r'read')
+        self.lexer.add('WRITE', r'write')
+        
+        #Outros
+        self.lexer.add('THEN', r'then')
         self.lexer.add('PROGRAM', r'program')   
         self.lexer.add('BEGIN', r'begin')    
+        self.lexer.add('IDENTIFIER', r'[a-zA-Z_][a-zA-Z0-9_]*')
 
     def get_lexer(self):
         self._add_tokens()
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from analisador_lexico import Analisador_Lexico
+
+text_input = """
+not 8
+"""
+lexer = Analisador_Lexico().get_lexer()
+tokens = lexer.lex(text_input)
+
+for token in tokens:
+    print(token)
